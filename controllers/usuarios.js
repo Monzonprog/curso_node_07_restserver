ffef6e9607441c34835a5c536cd0a5a1afbc3fb4
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-09-16 14:35:59 +0200

    Create middleware and use it in route

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,6 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
-const { validationResult } = require('express-validator');
 
 const usuariosGet = (req, res = response) => {
     const { q, nombre = 'No name', apikey, page = '1', limit } = req.query;
@@ -17,11 +16,6 @@ const usuariosGet = (req, res = response) => {
 };
 
 const usuariosPost = async (req, res) => {
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors})
-    }
 
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,14 +1,31 @@
 const { Router } = require('express');
-const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
+const {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete,
+    usuariosPatch,
+} = require('../controllers/usuarios');
 const { check } = require('express-validator');
-
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 router.get('/', usuariosGet);
 
-router.post('/',[
-    check('correo', 'El correo no es válido').isEmail(),
-], usuariosPost);
+router.post(
+    '/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('correo', 'El correo no es válido').isEmail(),
+        check(
+            'password',
+            'El password debe de ser de más de 6 letras'
+        ).isLength({ min: 6 }),
+        check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        validarCampos,
+    ],
+    usuariosPost
+);
 
 router.put('/:id', usuariosPut);
 
