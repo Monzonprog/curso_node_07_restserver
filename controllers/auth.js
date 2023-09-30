81d6687cdc4a417129800c56a7748384043ff4c9
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-09-30 20:51:02 +0200

    Create validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,46 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login ok',
-    });
+const login = async (req, res = response) => {
+    const { correo, password } = req.body;
+
+    try {
+        //Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - Correo',
+            });
+        }
+
+        //Si el usuario está activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos- Estado: false',
+            });
+        }
+
+        //Verificar contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos- password',
+            });
+        }
+
+        //Generar JWT
+        res.json({
+            msg: 'Login ok',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
 };
 
 module.exports = {
-    login
-}
+    login,
+};
