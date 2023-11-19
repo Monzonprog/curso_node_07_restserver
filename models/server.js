04f7cf63b067c8987ce0c79933d0c5868dfc3a34
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-11-19 13:36:09 +0100

    Create router and controller

--- models/server.js
+++ models/server.js
@@ -9,6 +9,7 @@ class Server {
 
         this.paths = {
             auth: '/api/auth',
+            buscar: '/api/buscar',
             usuarios: '/api/usuarios',
             productos: '/api/productos',
             categorias: '/api/categorias',
@@ -38,6 +39,7 @@ class Server {
 
     route() {
         this.app.use(this.paths.auth, require('../routes/auth'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
